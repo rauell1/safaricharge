bcfdd548decab3c6d36b606565cea8792ee99020
Roy Otieno | Energy & Mobility Systems <royokola3@gmail.com>
2026-04-12 21:52:22 +0300

    refactor: delete dashboard/ shim folder

--- src/components/dashboard/BatteryHealthCard.tsx
+++
@@ -1,3 +0,0 @@
-// Canonical: src/components/energy/BatteryHealthCard.tsx
-export { BatteryHealthCard } from '@/components/energy/BatteryHealthCard';
-export type { BatteryHealthCardProps } from '@/components/energy/BatteryHealthCard';
