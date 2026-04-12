29e19a795a25bc5c1feca2bdf979ff8b7c75d002
Roy Otieno | Energy & Mobility Systems <royokola3@gmail.com>
2026-04-12 09:45:30 +0300

    fix: remove non-existent react-hooks/set-state-in-effect ESLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,8 +18,7 @@ const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
     
     // React rules
     "react-hooks/exhaustive-deps": "off",
-    "react-hooks/purity": "off",
-    "react-hooks/set-state-in-effect": "off",
+    "react-hooks/rules-of-hooks": "off",
     "react/no-unescaped-entities": "off",
     "react/display-name": "off",
     "react/prop-types": "off",
